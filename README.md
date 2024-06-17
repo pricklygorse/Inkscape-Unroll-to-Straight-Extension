5b1547891d0894571bdd4ee0382a80d7d10b5d52
pricklygorse <seanwhitley2000@yahoo.com>
2024-06-17 22:47:20 +0100

    experimental offset to original shape

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Inkscape-Unroll-to-Straight-Extension
 
-Unroll a path/shape into a straight line, maintaining the node spacings that were on the original path. Segments can be straight or bezier curves.
+Unroll a path/shape into a straight line, maintaining the node spacings that were on the original path. Segments can be straight or bezier curves. Optionally you can add extrude and offset (e.g. add width and seam allowance) to the path.
 
 Has been tested most on Inkscape 1.2 and 1.3. *Should* work on 1.1, but I recommend updating. Inkscape has improved so much.
 
@@ -14,46 +14,43 @@ I plan to release more related extensions soon, which will all be placed under t
 
 ## Usage
 
-Select your path (or mulitple individual paths) and run the extension.
+Select your path (or multiple individual paths) and run the extension (Extensions - Prickly Gorse - Unroll Path to Straight Path).
 
 Shapes need to be converted to paths (Object - Object to Path).
 
 The extension can either generate a new straight path with the nodes at the same spacings, number the nodes, label segment lengths, extrude the line to a 2D shape, and add an offset to the extruded shape. You can chose the units, decimal precision, and label prefixes.
 
-------------------------------------------------------------------------
+![](images/lineexamples.png)
 
-An example usage is making a sewing pattern for a bag. You've drawn your side panels and you need to calculate how long your middle strip needs to be, and you want alignment marks along the middle strip piece that align with the side panel corners.
+![](images/extrude%20offset.png)
 
-![](images/bag%20example.png)
 
-Select your shape and run the extension (Extensions - Prickly Gorse - Unroll Path to Straight Path).
 
-This produces a line slightly above the original path's bounding box that is the total length of the path, with nodes placed the same distances along as on the original path. You can generate either a single line (A), a line and an extra group with numbered nodes (B) or labeled segment lengths in the units of your choice (C).
+
+This produces a line slightly above the original path's bounding box that is the total length of the path, with nodes placed the same distances along as on the original path. You can generate either a single line (A), a line and an extra group with numbered nodes (B) or labeled segment lengths in the units of your choice (C). Using the Cumulative Segment Lengths label option is handy for marking out on fabric/paper without needing to print the image itself.
 
 You can add a label prefix, for example if making a top and bottom line.
 
 Numbered nodes are useful if you can't remember which node aligns with which on the original shape. If you have drawn the original path in the wrong order and don't like your starting node, chose your new starting node and use the 'Break Path at Selected Nodes' tool, then re-run the extension. Or trace over your old shape with a new path, which is a bit more reliable if you want to keep it a closed shape.
 
-![](images/line%20examples.png)
 
 The extrude and offset features allow you to generate a full middle panel (gusset) with a seam allowance with almost zero effort. Select 'Extrude', and choose your bag width and seam allowance width.
 
-![](images/extrude%20offset.png)
-
 From here you could add a marker style to the nodes for easy alignment with the side panel when sewing.
 
-Using the Cumulative Segment Lengths label option is handy for marking out on fabric/paper without needing to print the image itself.
 
-## Another example
+
+## Another example: Top Tube Bag
 
 ![](images/example2.png)
 
 ## Prickly Gorse / MYOG tutorials Sewing Guides
 
-I wrote this to assist with making sewing patterns for bike and backpacking bags. Bit of a shameless plug, but if you are interested in sewing your own outdoor gear without making your own patterns, check out  [www.myogtutorials.com](http://www.myogtutorials.com) for MYOG sewing focused site (patterns and articles), [www.payhip.com/pricklygorse](http://www.payhip.com/pricklygorse) where my sewing guides are hosted, or [www.pricklygorsegear.com/tutorials-myog](http://www.pricklygorsegear.com/tutorials-myog) for my handmade bags website.
+I wrote this to assist with making sewing patterns for bike and backpacking bags. Bit of a shameless plug, but if you are interested in sewing your own outdoor gear without making your own patterns, check out  [www.myogtutorials.com](http://www.myogtutorials.com) for MYOG sewing focused site (patterns and articles), [www.payhip.com/pricklygorse](http://www.payhip.com/pricklygorse) where my sewing guides are hosted, or [www.pricklygorsegear.com](http://www.pricklygorsegear.com) for my handmade bags website.
 
 You can also donate at this [link](https://www.paypal.com/donate/?business=WBEASYMGED4X8) if you find it useful :)
 
 ## Acknowledgements
 
 The code is built upon the Measure Path and Number Nodes extensions that come pre-installed with Inkscape. Huge thanks to the authors of those extensions, and everyone who has contributed to the Inkscape project as a whole. Support open source!
+------------------------------------------------------------------------
--- unroll_to_straight.py
+++ unroll_to_straight.py
@@ -7,6 +7,15 @@ from inkex.bezier import csplength
 from inkex import TextElement, Circle, PathElement
 import string
 
+
+#imports for tangent
+from inkex.paths import Path, Curve, Move, Line, Quadratic, move, curve, line
+from inkex.transforms import Vector2d
+import math
+from math import atan2, degrees
+
+from inkex import PathEffect
+
 class MeasureLength(inkex.EffectExtension):
    
     
@@ -31,7 +40,9 @@ class MeasureLength(inkex.EffectExtension):
         pars.add_argument('--extrude', type=inkex.Boolean, default=False)
         pars.add_argument('--extrude_width', type=float, default=10.000)
         pars.add_argument('--offset', type=inkex.Boolean, default=False)
+        pars.add_argument('--copy_style', type=inkex.Boolean, default=True)
         pars.add_argument('--offset_distance', type=float, default=1.000)
+        pars.add_argument('--offset_shape', type=inkex.Boolean, default=False)
         pars.add_argument('--textsize', default="10px")
         pars.add_argument('--initial_n', default=1)
 
@@ -76,9 +87,8 @@ class MeasureLength(inkex.EffectExtension):
           
           #inkscape 1.2 and newer
           prec = int(self.options.precision)
-          scale = self.svg.viewport_to_unit(
-            "1" + self.svg.document_unit
-          )  # convert to document units
+          # convert to document units
+          scale = self.svg.viewport_to_unit("1" + self.svg.document_unit)  
           factor = self.svg.unit_to_viewport(1, self.options.unit)
 
         
@@ -89,51 +99,105 @@ class MeasureLength(inkex.EffectExtension):
         # loop over all selected paths
         filtered = self.svg.selection.filter(inkex.PathElement)
         
-        #how far to move path from the top of the bounding box
-        nudge = 15
+        
         
         if not filtered:
             raise inkex.AbortExtension(_("Please select at least one path object."))
         
+        
+        tick_style = inkex.Style(
+          {
+            "stroke": "red",
+            "stroke-width": self.svg.unittouu("0.5mm"),
+            "fill": "none",
+            #"stroke-dasharray": str(1/factor) + "," + str(1/factor)
+          }
+        )
+        border_style = inkex.Style(
+          {
+            "stroke": "black",
+            "stroke-width": self.svg.unittouu("0.5mm"),
+            "fill": "none",
+            #"stroke-dasharray": str(1/factor) + "," + str(1/factor)
+          }
+        )
+        seamline_style = inkex.Style(
+          {
+            "stroke": "black",
+            "stroke-width": self.svg.unittouu("0.5mm"),
+            "fill": "none",
+            "stroke-dasharray": "4,4"
+          }
+        )
+        
+       
+        
+        
+        
+        
+        
+        
         for node in filtered:
             
+            # Make a group for all the generated objects
+            current_layer = inkex.Group()
+            self.svg.get_current_layer().append(current_layer)
+            
+            
             csp = node.path.transform(node.composed_transform()).to_superpath()
-                        
             slengths, stotal = csplength(csp)
             
             # convert segment lengths into user defined units.
-            slengths_uu = []
+            slengths_uu = [round(number * factor, prec) for number in slengths[0]]
             cumulative = [0]
             count = 0
             for number in slengths[0]:
-                 length_user_unit = round(number * factor, prec)
-  
-                 slengths_uu.append(length_user_unit)
                  
                  count += number
                  
                  cumulative.append(round(count * factor,prec))
           
+            
+          
             # Position the new line at the top left corner of the bounding box
             # otherwise if you have multiple paths selected they can overlap
             # when using a fixed position.
             x_initial, y_initial = node.bounding_box().minimum
-            y_initial = y_initial - nudge # nudge it slightly away from the bounding box
+            y_initial -= 25 # nudge it slightly away from the bounding box
             
             
             # SVG code for the first point position
-            single_line_init = 'M ' +  str(x_initial) + ' ' + str(y_initial) + ' ' 
-            
+            svg_code = f'M {x_initial} {y_initial} '
+            svg_initial_line = ""
 
-            single_line_svg = ""
+            #single_line_svg = ""
+            ticks=""
+            
+            # Convert offset and extrude width to document units
+            off_val = self.options.offset_distance / factor
+            extrude_w_doc_u = self.options.extrude_width / factor
             
             # Draw a horizontal line relative to the previous one the length of
             # the segment
             for i in slengths[0]:
-                single_line_svg =  single_line_svg + "h "+ str(i) + " "
-              
-            svg_code = single_line_init + single_line_svg       
-      
+                svg_code += f'h {i} '
+                svg_initial_line += f'h {i} '
+                #alignment ticks
+                ticks += f'v {off_val} m {i},-{off_val} '
+                
+            
+            
+            
+            #svg_code = single_line_init + single_line_svg
+            
+            
+            #add the final tick
+            ticks += f'v {off_val}'
+            
+            #ticks_svg = single_line_init + ticks + single_line_init + "m 0," + str(-extrude_w_doc_u - off_val) + ticks
+            ticks_svg = f'M {x_initial} {y_initial} {ticks} M {x_initial},{y_initial - extrude_w_doc_u - off_val} {ticks}'
+            
+                  
             if self.options.extrude == True:
                # Convert width unit to document units
                extrude_w_doc_u = self.options.extrude_width / factor
@@ -142,17 +206,23 @@ class MeasureLength(inkex.EffectExtension):
                rev_line_svg = ""
                
                for i in list(reversed(slengths[0])):
-                   rev_line_svg =  rev_line_svg + "h "+ str(-i) + " "
+                   rev_line_svg +=  f"h {-i} "
                   
                # Make the path into the full extruded rectangle
-               svg_code = single_line_init + single_line_svg +" v " + str(-extrude_w_doc_u) + rev_line_svg + " z"
-            
+               #svg_code = single_line_init + single_line_svg +" v " + str(-extrude_w_doc_u) + rev_line_svg + " z"
+               svg_code += f' v {-extrude_w_doc_u} {rev_line_svg} z'
             
             # Make the actual path for the unrolled path.
             my_path = PathElement()
 
-            # copy style from the original path
-            my_path.style.update(node.style)
+            
+            if self.options.offset == True & self.options.extrude == True:
+                #set style to match my sewing patterns
+                my_path.style = seamline_style
+              
+            else:
+               # copy style from the original path
+               my_path.style.update(node.style)
             
             # to set the style manually:
             #my_path.style['stroke'] = 'blue'
@@ -163,7 +233,6 @@ class MeasureLength(inkex.EffectExtension):
             
            
             # add path to current layer
-            current_layer = self.svg.get_current_layer()
             current_layer.append(my_path)
             
             # Probably a neater way to do this, but for now just replace what is
@@ -181,7 +250,11 @@ class MeasureLength(inkex.EffectExtension):
                 # line, not the entire way around the path if it has been
                 # extruded
                 single_line = PathElement()
-                single_line.set('d', single_line_init + single_line_svg)
+                single_line.set('d', f'M {x_initial} {y_initial} {svg_initial_line}')
+                
+                
+                
+                
                 current_layer.append(single_line)
                                
                 self.add_dot(single_line, slengths_uu)
@@ -192,34 +265,35 @@ class MeasureLength(inkex.EffectExtension):
             
             if self.options.offset == True & self.options.extrude == True:
                 
-                # Convert offset to document units
+                
                 off_val = self.options.offset_distance / factor
                 
-                # Initial point, including offset
-                single_line_init = 'M ' +  str(x_initial - off_val) + ' ' + str(y_initial + off_val) + ' h ' + str(off_val)
+                if self.options.offset_shape:
+                    self.tangentlines(node,off_val)
+                    self.offsetLPE(node)
                 
-                svg_code = (single_line_init 
-                            + single_line_svg 
-                            + " h " 
-                            + str(off_val)
-                            + " v "
-                            + str( (2*-off_val) - extrude_w_doc_u)
-                            + " h " 
-                            + str(-off_val)
-                            + rev_line_svg
-                            + str(-off_val)
-                            +" z"
-                            )
+                
+                # Initial point, including offset
+                svg_code = (
+                    f'M {x_initial - off_val} {y_initial + off_val} h {off_val} '
+                    f'{svg_initial_line} h {off_val} v {-(2 * off_val + extrude_w_doc_u)} h {-off_val} {rev_line_svg}{-off_val} z'
+                )
+                
+   
                 my_path = PathElement()
 
                 # copy style from the original path
-                my_path.style.update(node.style)
-                #my_path.style['stroke'] = 'blue'
+                #my_path.style.update(node.style)
+                my_path.style = border_style
                 
                 my_path.set('d', svg_code     )
 
                 # add path to current layer
-                current_layer = self.svg.get_current_layer()
+                current_layer.append(my_path)
+                
+                my_path = PathElement()
+                my_path.style = tick_style
+                my_path.set('d', ticks_svg)
                 current_layer.append(my_path)
                 
                 
@@ -245,7 +319,7 @@ class MeasureLength(inkex.EffectExtension):
         group: inkex.Group = node.getparent().add(inkex.Group())
         dot_group = group.add(inkex.Group())
         num_group = group.add(inkex.Group())
-        # i think this is to account for layers having transforms, so when
+        # account for layers having transforms, so when
         # you add stuff to the document it is added relative to the layer transform,
         # not the absolute document X Y position
         path_trans_applied = node.path.transform(node.composed_transform())
@@ -265,7 +339,7 @@ class MeasureLength(inkex.EffectExtension):
             circle.style = style
 
             if self.options.adddots == "num":
-                 nodeLabel = str(1+step + (int(self.options.initial_n) - 1))
+                 nodeLabel = str(step + int(self.options.initial_n))
                 
             elif self.options.adddots == "alpha":
                  nodeLabel = string.ascii_uppercase[step]
@@ -274,20 +348,11 @@ class MeasureLength(inkex.EffectExtension):
             
             segLabel = ""
          
-            # ugly hack to bypass error when looking for a non-existent segment length for the last node.
-            # only applies for segment length labels, cumulative labels have a value for the last node.
-            try:
-                     
-                if not self.options.adddots == "no" and not self.options.addseglengths =="no":
-                    # user wants node numbering and segment lengths
-                    segLabel = ": "+ str(seglengths[step]) + self.options.unit
-                elif self.options.adddots == "no" and not self.options.addseglengths =="no":
-                    # user wants no node numbers but wants segment lengths
-                    segLabel =  str(seglengths[step]) + self.options.unit
-            except Exception:
-                # We are at the last node and there is no segment length for the last node.
-                segLabel=""
-                pass
+            if step < len(seglengths):
+                if not self.options.adddots == "no" and not self.options.addseglengths == "no":
+                    segLabel = f": {seglengths[step]}{self.options.unit}"
+                elif self.options.adddots == "no" and not self.options.addseglengths == "no":
+                    segLabel = f"{seglengths[step]}{self.options.unit}"
             
             dot_text = self.options.label_prefix + nodeLabel + segLabel
             
@@ -298,6 +363,188 @@ class MeasureLength(inkex.EffectExtension):
                     dot_text,
                 )
             )
+
+
+
+
+
+
+
+
+
+
+
+    def tangentlines(self, node,off_val):
+        
+        #for node in self.svg.selection.filter(inkex.PathElement):
+            
+            
+            
+            result = Path()
+    
+            prev = Vector2d()
+            start = None
+            
+                       
+           
+            node2 = node.copy()
+            node2.path = node2.get_path().to_superpath().to_path(curves_only=True)
+            
+            #self.msg(node.path)
+            #self.msg(node2.path)
+            
+            for seg in node2.path.to_relative():
+    
+                #self.msg("")
+                
+                
+                if start is None:
+                    start = seg.end_point(start, prev)
+                    
+                
+                
+                #self.msg((seg))
+                #self.msg(dir(seg))
+                
+                # the superpath transform seems to make the last node the same as previous.
+                # C 0 0 0 0 0, so this checks to avoid that segment
+                if isinstance(seg, curve) and not (seg.dx2+seg.dx3+seg.dx4+seg.dy2+seg.dy3+seg.dy4)==0:
+                    
+    
+                    line_length=off_val
+                    
+                    #if the first node is a corner node with no handle,
+                    # use the second handle as the reference point 
+                    if seg.dy2 == 0 and seg.dx2 == 0:
+                        ang = atan2(seg.dy3 , seg.dx3 )
+                        
+                    else:
+                        ang = atan2(seg.dy2 , seg.dx2 )
+    
+                    x_end =  line_length * math.cos(ang)
+                    y_end =  line_length * math.sin(ang)
+                    
+                    
+                    
+                    #if the second node is a corner node with no handle,
+                    # use the first handle as the reference point 
+                    if seg.dy3 == seg.dy4 and seg.dx3 == seg.dx4:
+                        dy3 = seg.dy2
+                        dx3 = seg.dx2
+                    else:
+                        dy3 = seg.dy3
+                        dx3 = seg.dx3
+                    
+                    ang2 = atan2(dy3-seg.dy4 , dx3-seg.dx4 )
+                    
+                    x_end2 =  line_length * math.cos(ang2)
+                    y_end2 =  line_length * math.sin(ang2)
+                    
+                    #self.msg(degrees(ang2))
+                    #self.msg(y_end2)
+                    
+                    result += [
+                        
+                        Move(prev.x, prev.y),
+                        line(y_end, -x_end),
+                        
+    
+                        #go back
+                        move(-y_end, x_end),
+                        
+                        
+                        move(seg.dx4, seg.dy4),
+                        line(-(y_end2), (x_end2)),
+                        
+                        
+                        
+                        #original extension code
+                        # Move(seg.x2, seg.y2),
+                        # Line(prev.x, prev.y),
+                        # Move(seg.x3, seg.y3),
+                        # Line(seg.x4, seg.y4),
+                        
+                        
+                        
+                        
+                    ]
+                # elif isinstance(seg, Quadratic):
+                #     result += [
+                #         Move(seg.x2, seg.y2),
+                #         Line(prev.x, prev.y),
+                #         Move(seg.x2, seg.y2),
+                #         Line(seg.x3, seg.y3),
+                #     ]
+                prev = seg.end_point(start, prev)
+                
+    
+            #if not result:
+            #    continue
+    
+            elem = node.getparent().add(inkex.PathElement())
+            elem.path = result.transform(node.transform)
+            elem.style = {
+                "stroke-linejoin": "miter",
+                "stroke-width": "0.5",
+                "stroke-opacity": "1.0",
+                "fill-opacity": "1.0",
+                "stroke": "red",
+                "stroke-linecap": "butt",
+                "fill": "none",
+            }
+
+
+
+
+    def offsetLPE(self, node):
+            
+            node2 = node.copy()
+            
+            node2.style = {
+                "stroke-linejoin": "miter",
+                "stroke-width": "0.5",
+                "stroke-opacity": "1.0",
+                "fill-opacity": "1.0",
+                "stroke": "blue",
+                "stroke-linecap": "butt",
+                "fill": "none",
+            }
+            
+            self.svg.get_current_layer().append(node2)
+    
+            #offset is a parameter from the extension ui.
+            #offset = self.options.offset
+            offset_param_dict = {"update_on_knot_move": "true", 
+                                      "attempt_force_join": "false", 
+                                      "miter_limit": "4", 
+                                      "offset": self.options.offset_distance, 
+                                      "unit": self.options.unit,
+                                      "linejoin_type": "miter",
+                                      "lpeversion": "1.2",
+                                      "is_visible": "true",
+                                      "effect": "offset"}
+    
+            #for p in paths_element:
+            effect = PathEffect()
+            for key in offset_param_dict:
+                effect.set(key, offset_param_dict[key])          
+
+            self.svg.defs.add(effect)
+            node2.set('inkscape:original-d', node2.attrib["d"])
+            node2.set('inkscape:path-effect', effect.get_id(as_url = 1))   
+            node2.pop('d')
+
+
+
+
+
+
+
+
+
+
+
+
 
            
 if __name__ == "__main__":
